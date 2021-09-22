e1ea651d2ffc314d0a62bb7b01693e1572598230
Abel Asres <abelasres@gmail.com>
2021-09-21 22:58:06 -0400

    all operands work

--- Calculator_App/app/src/main/java/com/map524/calculator/Calculator.java
+++ Calculator_App/app/src/main/java/com/map524/calculator/Calculator.java
@@ -1,41 +1,49 @@
 package com.map524.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.util.Log;
+
+    import java.util.ArrayList;
+    import java.util.List;
 
 public class Calculator {
 
     private List <String> numbers_operands = new ArrayList<String>();
     void push(String value) {
         numbers_operands.add(value);
-
    }
 
    int calculate() {
         int result = Integer.parseInt(numbers_operands.get(0));
         numbers_operands.remove(0);
-        int count = 0;
+        //Log.d(null, String.valueOf(numbers_operands.isEmpty()));
+        //int count = 0;
+       for (String i: numbers_operands) {
+           Log.d(null, i);
+       }
         while (numbers_operands.size() > 0) {
             //check for operand
             //perform opertaion with the next value
             //remove values from array
-            if (numbers_operands.get(0) == "+") {
+            Log.d(null,numbers_operands.get(0));
+            if (numbers_operands.get(0).equals("+")) {
                 result+=Integer.parseInt(numbers_operands.get(1));
+                Log.d(null,Integer.toString(result));
                 numbers_operands.remove(0);
-                numbers_operands.remove(1);
-            } else if (numbers_operands.get(0) == "-") {
+                numbers_operands.remove(0);
+            } else if (numbers_operands.get(0).equals("-")) {
                 result-=Integer.parseInt(numbers_operands.get(1));
                 numbers_operands.remove(0);
-                numbers_operands.remove(1);
-            } else if(numbers_operands.get(0) == "*") {
+                numbers_operands.remove(0);
+            } else if(numbers_operands.get(0).equals("*")) {
                 result*=Integer.parseInt(numbers_operands.get(1));
                 numbers_operands.remove(0);
-                numbers_operands.remove(1);
+                numbers_operands.remove(0);
             } else {
                 result/=Integer.parseInt(numbers_operands.get(1));
                 numbers_operands.remove(0);
-                numbers_operands.remove(1);
+                numbers_operands.remove(0);
             }
+            //Log.d(null, String.valueOf(result));
         }
         return result;
    }
--- Calculator_App/app/src/main/java/com/map524/calculator/MainActivity.java
+++ Calculator_App/app/src/main/java/com/map524/calculator/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Calculator calculator = new Calculator();
+    String number = "";
+    Boolean clear_display=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,28 +23,43 @@ public class MainActivity extends AppCompatActivity {
         button = (Button)view;
         TextView display_view = (TextView) findViewById(R.id.display);
 
+
         String clickedValue = button.getText().toString();
         Log.d(null,clickedValue);
-        if (clickedValue.equals("=")) {
 
-            Log.d(null,"checking if =");
-            if (calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("+")
-            || calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("-")
-            || calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("*")
-            || calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("/")) {
+        if (clickedValue.equals("+") || clickedValue.equals("-") || clickedValue.equals("*") || clickedValue.equals("/")) {
             //create a alert dialog due to error (cannot end with operand)
-
-                Log.d(null,"cannot perform operation");
-            } else {
+            Log.d(null,"click operand");
+            calculator.push(number);
+            calculator.push(clickedValue);
+            display_view.setText(display_view.getText().toString()+clickedValue);
+            number="";
+        } else if (clickedValue.equals("=")) {
+            Log.d(null,"clicked =");
+            //if (calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("+")
+            //|| calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("-")
+            //|| calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("*")
+            //|| calculator.getNumberOperands().get(calculator.getNumberOperands().size()).equals("/")) {
+            //create a alert dialog due to error (cannot end with operand)
+            //    Log.d(null,"cannot perform operation");
+            //} else {
                 Log.d(null,"before calculation is performed");
+                calculator.push(number);
+                number="";
+                clear_display=true;
                 //perform calculation
                 int result;
                 result = calculator.calculate();
-            }
-
+                display_view.setText(Integer.toString(result));
+            //}
         } else {
+            if (clear_display) {
+                display_view.setText("");
+                clear_display=false;
+            }
             Log.d(null,"calculator push");
-            calculator.push(clickedValue);
+            number+=clickedValue;
+            Log.d(null,number);
             display_view.setText(display_view.getText().toString()+clickedValue);
         }
     }
