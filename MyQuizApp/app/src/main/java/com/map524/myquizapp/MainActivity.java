614701cafca5013da986a2038f6ad1242ac2f68d
Abel Asres <abelasres@gmail.com>
2021-11-23 22:29:47 -0500

    fragments change with snackbar update

--- MyQuizApp/app/src/main/java/com/map524/myquizapp/MainActivity.java
+++ MyQuizApp/app/src/main/java/com/map524/myquizapp/MainActivity.java
@@ -6,48 +6,89 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TextView quizQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getFragmentManager();
+
+        WelcomeFragment welcomeFragObj = (WelcomeFragment) fm.findFragmentById(R.id.quiz_fragment);
+
+        FragmentTransaction transaction = fm.beginTransaction();
+
+        // that mean the area is empty
+        // I'm able to add fragment here
+        WelcomeFragment welcomeFragment = new WelcomeFragment();
+        transaction.add(R.id.quiz_fragment, welcomeFragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.commit();
     }
 
 
-    public void checkAnswer(View view){
-
+    public void startQuiz(View view){
+        //Remove welcome screen
         FragmentManager fm = getFragmentManager();
-
-
-        QuizFragment quizFragObj = (QuizFragment) fm.findFragmentById(R.id.quiz_fragment);
-
+        WelcomeFragment welcomeFragObj = (WelcomeFragment) fm.findFragmentById(R.id.quiz_fragment);
         FragmentTransaction transaction = fm.beginTransaction();
+        transaction.remove(welcomeFragObj);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
 
-        if(quizFragObj == null){
-            // that mean the area is empty
-            // I'm able to add fragment here
-            QuizFragment quizFragment = new QuizFragment();
-            transaction.add(R.id.quiz_fragment, quizFragment);
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        }else{
-            transaction.remove(quizFragObj);
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+
+        //start quiz
+        QuizFragment quizFragment = new QuizFragment();
+
+
+        quizFragment.setQuestion(1);
+
+        //quizQuestion = findViewById(R.id.quizText);
+
+        transaction.add(R.id.quiz_fragment, quizFragment);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.commit();
+        Snackbar.make(findViewById(R.id.quiz_fragment), "You are starting the quiz",
+                Snackbar.LENGTH_SHORT)
+                .show();
+    }
+
+
+    public void checkAnswer (View view){
+
+        switch (view.getId()){
+            case R.id.trueButton:
+                // check answer if it was correct
+                break;
+            case R.id.falseButton:
+                //check if answer was correct
+                break;
+            default:
+                throw new RuntimeException("Unknown button ID");
         }
 
-        transaction.commit();
 
-
-
-
-        /*QuizFragment nextQuizFragment = new QuizFragment();
         FragmentManager fm = getFragmentManager();
+        QuizFragment quizFragObj = (QuizFragment) fm.findFragmentById(R.id.quiz_fragment);
         FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.quiz_fragment,QuizFragment.class);
+        QuizFragment quizFragment = new QuizFragment();
+        quizFragment.setQuestion(quizFragObj.getQuestion() + 1);
+        transaction.replace(R.id.quiz_fragment, quizFragment);
         transaction.commit();
-        */
+        Snackbar.make(findViewById(R.id.quiz_fragment), "You completed questin: "+quizFragObj.getQuestion(),
+                Snackbar.LENGTH_SHORT)
+                .show();
+        //quizQuestion = findViewById(R.id.quizText);
+        //quizQuestion.setText(Integer.toString(quizFragment.getQuestion()));
+        //Log.d("quiz", "checkAnswer: "+quizFragment.getQuestion());
+        //track progress
     }
 }
--- MyQuizApp/app/src/main/java/com/map524/myquizapp/QuizFragment.java
+++ MyQuizApp/app/src/main/java/com/map524/myquizapp/QuizFragment.java
@@ -2,18 +2,36 @@ package com.map524.myquizapp;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class QuizFragment extends Fragment {
+    int question;
+    TextView questionText;
+
+
+    public int getQuestion() {
+        return question;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz,container,false);
+        View v =inflater.inflate(R.layout.fragment_quiz,container,false);
+        this.questionText = v.findViewById(R.id.quizText);
+        this.questionText.setText(Integer.toString(this.question));
+        return v;
+    }
+
+    public void setQuestion(int question) {
+
+        this.question = question;
     }
 }
